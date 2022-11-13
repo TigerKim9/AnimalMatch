615cb53c5cc033e54d32f049c7343b69fb790a2d
TigerKim9 <kkm9499@gmail.com>
2022-11-13 23:21:32 +0900

    create API and constructor study

--- AnimalMatch/src/main/java/com/animalmatch/user/controller/UserAPIController.java
+++ AnimalMatch/src/main/java/com/animalmatch/user/controller/UserAPIController.java
@@ -1,8 +1,22 @@
 package com.animalmatch.user.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.animalmatch.user.User;
+import com.animalmatch.user.mapper.UserMapper;
 
 @RestController
 public class UserAPIController {
+	
+	UserMapper userMapper;
+	
+	
+	//User 받아 회원등록
+	@PostMapping("/addUser")
+	public int addUser(User user) {
+		int result = userMapper.addUser(user);
+		return result;
+	}
 
 }
